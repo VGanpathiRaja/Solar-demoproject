335ae6d830f1fcb43591c438a3f544f337f6e796
Ganapathi Raja <vganpathiraja96@gmail.com>
2024-07-06 17:08:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import OffcanvasExample from './Menubar/Header';
+import Firstsection from './Sections/Firstsection';
+import Secondsec from './Sections/Secondsec';
+import Thirdsection from './Sections/Thirdsection';
+import Fourthsection from './Sections/Fourthsection';
+import Fifthsection from './Sections/Fifthsection';
+import Sixthsection from './Sections/Sixthsection';
+import Seventhsection from './Sections/Seventhsection';
+import Eightsection from './Sections/Eightsection';
+function App() {
+  return (
+    <div>
+      <OffcanvasExample/>
+      <Firstsection/>
+      <Secondsec/>
+      <Thirdsection/>
+      <Fourthsection/>
+      <Fifthsection/>
+      <Sixthsection/>
+      <Seventhsection/>
+      <Eightsection/>
+    </div>
+  );
+}
+
+export default App;
+
